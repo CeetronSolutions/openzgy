1785fa474bd37f36c738189d685f616c91a4ac06
Jon Jenssen <jon@soundsoft.no>
2023-02-15 17:38:48 +0100

    Work in progress

--- include/zgyaccess/seismicslice.h
+++ include/zgyaccess/seismicslice.h
@@ -11,17 +11,16 @@ class SeismicSliceData
 {
 
 public:
-    SeismicSliceData();
-    ~SeismicSliceData();
+    SeismicSliceData(int width, int depth);
 
+    void addSubSliceData(int fromX, int fromY, int fromWidth, const std::vector<float> values, int toX, int toY);
+
+    const std::vector<float> values();
 
 private:
-    int m_nTraces;
-    int m_nSamplesInDepth;
-
-
+    int m_width;
+    int m_depth;
     std::vector<float> m_values;
-
 };
 
 
